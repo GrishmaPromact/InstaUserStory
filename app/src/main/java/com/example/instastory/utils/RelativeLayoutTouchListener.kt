e4d7051d2cd7efd0c287d66624beb37f746e2fbd
GrishmaPromact <varshil@saeculumsolutions.com>
2021-04-13 12:44:09 +0530

    RelativeLayoutTouchListener changes

--- app/src/main/java/com/example/instastory/utils/RelativeLayoutTouchListener.kt
+++ app/src/main/java/com/example/instastory/utils/RelativeLayoutTouchListener.kt
@@ -5,15 +5,16 @@ import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import android.view.View.OnTouchListener
+import android.widget.Button
 import android.widget.Toast
 import androidx.fragment.app.FragmentActivity
-import com.example.instastory.screen.BottomSheet
+import kotlin.math.abs
 
 
 class RelativeLayoutTouchListener(
-    mainActivity: FragmentActivity
+        mainActivity: FragmentActivity
 ) :
-    OnTouchListener {
+        OnTouchListener {
     private val activity: Activity
     private var downX = 0f
     private var downY = 0f
@@ -47,6 +48,57 @@ class RelativeLayoutTouchListener(
         onSwipeUpListener.onSwipeUp()
     }
 
+    companion object {
+        const val logTag = "ActivitySwipeDetector"
+        const val MIN_DISTANCE =
+                100 // TODO change this runtime based on screen resolution. for 1920x1080 is to small the 100 distance
+    }
+
+    // private MainActivity mMainActivity;
+    init {
+        activity = mainActivity
+    }
+
+    interface OnSwipeUp {
+        fun onSwipeUp()
+    }
+
+    fun setOnSwipeUpListener(listener: OnSwipeUp) {
+        onSwipeUpListener = listener
+    }
+
+   /* override fun onTouch(v: View, event: MotionEvent): Boolean {
+        val len = activity.resources.displayMetrics.densityDpi/6;
+        var initX: Float = 0f
+        var initY: Float = 0f
+        when (event.action) {
+
+            MotionEvent.ACTION_DOWN -> {
+                initX = event.x
+                initY = event.y
+            }
+            MotionEvent.ACTION_UP -> {
+                initX -= event.x //now has difference value
+                initY -= event.y //now has difference value
+                if (initY > len) {
+                    //(v as Button).text = "Swipe up"
+                    Toast.makeText(activity, "Swipe up", Toast.LENGTH_SHORT).show();
+                } else if (initY < -len) {
+                    //(v as Button).text = "Swipe Down"
+                    Toast.makeText(activity, "Swipe Down", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (initY < 0) initY = -initY
+                    if (initX < 0) initX = -initX
+                    if (initX <= len / 4 && initY <= len / 4) {
+                        //(v as Button).text = "Clicked"
+                        Toast.makeText(activity, "clicked...", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+        return true
+    }*/
+
     override fun onTouch(v: View, event: MotionEvent): Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
@@ -61,7 +113,7 @@ class RelativeLayoutTouchListener(
                 val deltaY = downY - upY
 
                 // swipe horizontal?
-                if (Math.abs(deltaX) > MIN_DISTANCE) {
+                if (abs(deltaX) > MIN_DISTANCE) {
                     // left or right
                     if (deltaX < 0) {
                         onLeftToRightSwipe()
@@ -73,14 +125,14 @@ class RelativeLayoutTouchListener(
                     }
                 } else {
                     Log.i(
-                        logTag,
-                        "Swipe was only " + Math.abs(deltaX) + " long horizontally, need at least " + MIN_DISTANCE
+                            logTag,
+                            "Swipe was only " + Math.abs(deltaX) + " long horizontally, need at least " + MIN_DISTANCE
                     )
                     // return false; // We don't consume the event
                 }
 
                 // swipe vertical?
-                if (Math.abs(deltaY) > MIN_DISTANCE) {
+                if (abs(deltaY) > MIN_DISTANCE) {
                     // top or down
                     if (deltaY < 0) {
                         onTopToBottomSwipe()
@@ -92,8 +144,8 @@ class RelativeLayoutTouchListener(
                     }
                 } else {
                     Log.i(
-                        logTag,
-                        "Swipe was only " + Math.abs(deltaX) + " long vertically, need at least " + MIN_DISTANCE
+                            logTag,
+                            "Swipe was only " + Math.abs(deltaX) + " long vertically, need at least " + MIN_DISTANCE
                     )
                     // return false; // We don't consume the event
                 }
@@ -101,25 +153,6 @@ class RelativeLayoutTouchListener(
             } // case MotionEvent.ACTION_UP:
         }
         return false
-    }
-
-    companion object {
-        const val logTag = "ActivitySwipeDetector"
-        const val MIN_DISTANCE =
-            100 // TODO change this runtime based on screen resolution. for 1920x1080 is to small the 100 distance
-    }
-
-    // private MainActivity mMainActivity;
-    init {
-        activity = mainActivity
-    }
-
-    interface OnSwipeUp{
-        fun onSwipeUp()
-    }
-
-    fun setOnSwipeUpListener(listener:OnSwipeUp) {
-        onSwipeUpListener = listener
     }
 
 }
